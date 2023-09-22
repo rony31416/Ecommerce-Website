e5548a90632d1f4c296366305c117b697f75b005
rony31416 <ronymajumder1171@gmail.com>
2023-09-23 02:50:01 +0600

    first commit

---
+++ demo.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element
+const button = document.querySelector('.button');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    // Display an alert when the button is clicked
+    alert('Button clicked!');
+});
